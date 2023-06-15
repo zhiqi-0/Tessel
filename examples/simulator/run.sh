49b3acfa34618c083f867419de27a210b726cc28
Zhiqi Lin <zhiqi.0@outlook.com>
2023-06-15 00:50:34 -0700

    update script

--- examples/simulator/run.sh
+++ examples/simulator/run.sh
@@ -8,22 +8,22 @@ mkdir -p $LOGS
 # VShape
 python examples/simulator/cases_tetris.py \
     --premise vshape --ndevs 4 --nmicros 4 --inflight 4 \
-2>&1 | tee -a ${LOGS}/tetris.vshape.log
+2>&1 | tee ${LOGS}/tetris.vshape.log
 
 # XShape
 python examples/simulator/cases_tetris.py \
-    --premise chimera --ndevs 4 --nmicros 3 --inflight 4 \
-2>&1 | tee -a ${LOGS}/tetris.xshape.log
+    --premise xshape --ndevs 4 --nmicros 3 --inflight 4 \
+2>&1 | tee ${LOGS}/tetris.xshape.log
 
 # MShape
 python examples/simulator/cases_tetris.py \
     --premise mshape --ndevs 4 --nmicros 6 --inflight 6 \
-2>&1 | tee -a ${LOGS}/tetris.mshape.log
+2>&1 | tee ${LOGS}/tetris.mshape.log
 
 # >|Shape
 python examples/simulator/cases_tetris.py \
     --premise yshape --ndevs 4 --nmicros 2 --inflight 4 \
-2>&1 | tee -a ${LOGS}/tetris.yshape.log
+2>&1 | tee ${LOGS}/tetris.yshape.log
 
 
 # ==================================================
@@ -31,19 +31,19 @@ python examples/simulator/cases_tetris.py \
 # VShape
 python examples/simulator/cases_full.py \
     --premise vshape --ndevs 4 --nmicros 4 --inflight 4 \
-2>&1 | tee -a ${LOGS}/naive.vshape.log
+2>&1 | tee ${LOGS}/naive.vshape.log
 
 # XShape
 python examples/simulator/cases_full.py \
     --premise xshape --ndevs 4 --nmicros 3 --inflight 4 \
-2>&1 | tee -a ${LOGS}/naive.xshape.log
+2>&1 | tee ${LOGS}/naive.xshape.log
 
 # MShape
 python examples/simulator/cases_full.py \
     --premise mshape --ndevs 4 --nmicros 6 --inflight 6 \
-2>&1 | tee -a ${LOGS}/naive.mshape.log
+2>&1 | tee ${LOGS}/naive.mshape.log
 
 # >|Shape
 python examples/simulator/cases_full.py \
     --premise yshape --ndevs 4 --nmicros 2 --inflight 4 \
-2>&1 | tee -a ${LOGS}/naive.yshape.log
+2>&1 | tee ${LOGS}/naive.yshape.log
