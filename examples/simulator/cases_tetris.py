6caa167b417de63cadda380ac5fcce7f708912ff
Zhiqi Lin <zhiqi.0@outlook.com>
2023-06-25 00:10:42 -0700

    update cases

--- examples/simulator/cases_tetris.py
+++ examples/simulator/cases_tetris.py
@@ -15,7 +15,7 @@ BW='backward'
 class Premise:
 
     @staticmethod
-    def vshape(ndevs: int) -> SchedPlan:
+    def vshape(ndevs: int, train: bool = True) -> SchedPlan:
         """
         f             b
           f         b  
@@ -25,15 +25,18 @@ class Premise:
         sched = SchedPlan(ndevs)
         fblocks = [Block(0, span=1, memory=1, btype=FW) for _ in range(ndevs)]
         fdevs = [[devid] for devid in range(ndevs)]
-        bblocks = [Block(0, span=3, memory=-1, btype=BW) for _ in range(ndevs)]
-        bdevs = [[devid] for devid in range(ndevs)][::-1]
+        if train:
+            bblocks = [Block(0, span=3, memory=-1, btype=BW) for _ in range(ndevs)]
+            bdevs = [[devid] for devid in range(ndevs)][::-1]
+        else:
+            bblocks, bdevs = [], []
         blocks = fblocks + bblocks
         devs = fdevs + bdevs
         sched.add_block_seq(blocks, devs)
         return sched
 
     @staticmethod
-    def xshape(ndevs: int) -> SchedPlan:
+    def xshape(ndevs: int, train: bool = True) -> SchedPlan:
         """
         f     f b     b
           f f     b b  
@@ -44,15 +47,21 @@ class Premise:
         # v
         fblocks = [Block(0, span=1, memory=1, btype=FW) for _ in range(ndevs)]
         fdevs = [[devid] for devid in range(ndevs)]
-        bblocks = [Block(0, span=3, memory=-1, btype=BW) for _ in range(ndevs)]
-        bdevs = [[devid] for devid in range(ndevs)][::-1]
+        if train:
+            bblocks = [Block(0, span=3, memory=-1, btype=BW) for _ in range(ndevs)]
+            bdevs = [[devid] for devid in range(ndevs)][::-1]
+        else:
+            bblocks, bdevs = [], []
         vblocks = fblocks + bblocks
         vdevs = fdevs + bdevs
         # ^
         fblocks = [Block(0, span=1, memory=1, btype=FW) for _ in range(ndevs)]
         fdevs = [[devid] for devid in range(ndevs)][::-1]
-        bblocks = [Block(0, span=3, memory=-1, btype=BW) for _ in range(ndevs)]
-        bdevs = [[devid] for devid in range(ndevs)]
+        if train:
+            bblocks = [Block(0, span=3, memory=-1, btype=BW) for _ in range(ndevs)]
+            bdevs = [[devid] for devid in range(ndevs)]
+        else:
+            bblocks, bdevs = [], []
         rvblocks = fblocks + bblocks
         rvdevs = fdevs + bdevs
 
@@ -61,7 +70,7 @@ class Premise:
         return sched
 
     @staticmethod
-    def interlace_s2s(ndevs: int) -> SchedPlan:
+    def nnshape(ndevs: int, train: bool = True) -> SchedPlan:
         """
         f f   f         b   b b
         f   f f         b b   b
@@ -69,21 +78,25 @@ class Premise:
         f     f   f b   b     b
         """
         sched = SchedPlan(ndevs)
-        # 
+        # v-shape
         fblocks = [Block(0, span=1, memory=1, btype=FW) for _ in range(ndevs)]
         fdevs = [[devid] for devid in range(ndevs)]
-        bblocks = [Block(0, span=2, memory=-1, btype=BW) for _ in range(ndevs)]
-        bdevs = [[ndevs-1-devid] for devid in range(ndevs)]
-        #
-        fblocks.insert(ndevs // 2, Block(0, span=1, memory=1, btype=FW))
+        bblocks, bdevs = [], []
+        if train:
+            bblocks = [Block(0, span=3, memory=-1, btype=BW) for _ in range(ndevs)]
+            bdevs = [[ndevs-1-devid] for devid in range(ndevs)]
+        # full shard 2
+        fblocks.insert(ndevs // 2, Block(0, span=1, memory=0, btype=FW))
         fdevs.insert(ndevs // 2, list(range(ndevs)))
-        bblocks.insert(ndevs // 2, Block(0, span=2, memory=-1, btype=BW))
-        bdevs.insert(ndevs // 2, list(range(ndevs)))
-        #
-        fblocks.insert(0, Block(0, span=1, memory=1, btype=FW))
+        if train:
+            bblocks.insert(ndevs // 2, Block(0, span=1, memory=0, btype=BW))
+            bdevs.insert(ndevs // 2, list(range(ndevs)))
+        # full shard 1
+        fblocks.insert(0, Block(0, span=1, memory=0, btype=FW))
         fdevs.insert(0, list(range(ndevs)))
-        bblocks.insert(len(bblocks), Block(0, span=2, memory=-1, btype=BW))
-        bdevs.insert(len(bblocks), list(range(ndevs)))
+        if train:
+            bblocks.insert(len(bblocks), Block(0, span=1, memory=0, btype=BW))
+            bdevs.insert(len(bblocks), list(range(ndevs)))
     
         blocks = fblocks + bblocks
         devs = fdevs + bdevs
@@ -121,7 +134,7 @@ class Premise:
         return sched
     
     @staticmethod
-    def mshape(ndevs: int) -> SchedPlan:
+    def mshape(ndevs: int, train: bool = False) -> SchedPlan:
         """
         f f             b b
         f   f         b   b
@@ -132,13 +145,17 @@ class Premise:
         # 
         fblocks = [Block(0, span=1, memory=1, btype=FW) for _ in range(ndevs)]
         fdevs = [[devid] for devid in range(ndevs)]
-        bblocks = [Block(0, span=3, memory=-1, btype=BW) for _ in range(ndevs)]
-        bdevs = [[ndevs-1-devid] for devid in range(ndevs)]
+        if train:
+            bblocks = [Block(0, span=3, memory=-1, btype=BW) for _ in range(ndevs)]
+            bdevs = [[ndevs-1-devid] for devid in range(ndevs)]
+        else:
+            bblocks, bdevs = [], []
         # fully shard
         fblocks.insert(0, Block(0, span=1, memory=0, btype=FW))
         fdevs.insert(0, list(range(ndevs)))
-        bblocks.insert(len(bblocks), Block(0, span=1, memory=0, btype=BW))
-        bdevs.insert(len(bblocks), list(range(ndevs)))
+        if train:
+            bblocks.insert(len(bblocks), Block(0, span=1, memory=0, btype=BW))
+            bdevs.insert(len(bblocks), list(range(ndevs)))
     
         blocks = fblocks + bblocks
         devs = fdevs + bdevs
@@ -146,90 +163,42 @@ class Premise:
         return sched
 
     @staticmethod
-    def mshape_imbalance(ndevs: int) -> SchedPlan:
+    def kshape(ndevs: int, train: bool = True) -> SchedPlan:
         """
-        f f             b b
-            f         b    
-        f     f     b     b
-                f b        
-        """
-        sched = SchedPlan(ndevs)
-        # 
-        fblocks = [Block(0, span=1, memory=1, btype=FW) for _ in range(ndevs)]
-        fdevs = [[devid] for devid in range(ndevs)]
-        bblocks = [Block(0, span=3, memory=-1, btype=BW) for _ in range(ndevs)]
-        bdevs = [[ndevs-1-devid] for devid in range(ndevs)]
-        #
-        fblocks.insert(0, Block(0, span=1, memory=1, btype=FW))
-        fdevs.insert(0, list(range(0, ndevs, 2)))
-        bblocks.insert(len(bblocks), Block(0, span=1, memory=-1, btype=BW))
-        bdevs.insert(len(bblocks), list(range(0, ndevs, 2)))
-
-        blocks = fblocks + bblocks
-        devs = fdevs + bdevs
-        sched.add_block_seq(blocks, devs)
-        return sched
-    
-    @staticmethod
-    def two_tower(ndevs: int) -> SchedPlan:
-        """
-          f f b-b b-b
-          f f b-b b-b
-        f   f b-b     b-b
-          f f b-b b-b
+        f   f b   b
+          f f b b  
+        f   f b   b
+          f f b b
         """
         sched = SchedPlan(ndevs)
         # full spmd on loss:
-        loss = [Block(0, span=1, memory=1, btype=FW), Block(0, span=1, memory=-1, btype=BW)]
-        loss_devs = [list(range(ndevs)), list(range(ndevs))]
-        # spmd
-        ndevs1 = ndevs // 2
-        fbranch1 = [Block(0, span=1, memory=1, btype=FW)]
-        fdevs = [list(range(ndevs1))]
-        bbranch1 = [Block(0, span=2, memory=-1, btype=BW)]
-        bdevs = [list(range(ndevs1))]
-        branch1 = fbranch1 + [None] * (ndevs-ndevs1-1) + loss + bbranch1
-        branch1_devs = fdevs + [None] * (ndevs-ndevs1-1) + loss_devs + bdevs
-    
-        # vshape
-        ndevs2 = ndevs - ndevs1
-        fblocks = [Block(0, span=1, memory=1, btype=FW) for _ in range(ndevs2)]
-        fdevs = [[devid+ndevs1] for devid in range(ndevs2)]
-        bblocks = [Block(0, span=2, memory=-1, btype=BW) for _ in range(ndevs2)]
-        bdevs = [[devid+ndevs1] for devid in range(ndevs2)][::-1]
-        branch2 = fblocks + loss + bblocks
-        branch2_devs = fdevs + loss_devs + bdevs
-    
-        print(branch1)
-        print(branch2)
-        sched.add_block_seq(branch2, branch2_devs)
-        sched.add_block_seq(branch1, branch1_devs)
-        return sched
-
-    @staticmethod
-    def yshape(ndevs: int) -> SchedPlan:
-        """
-        f   f
-          f f
-        f   f
-          f f 
-        """
-        sched = SchedPlan(ndevs)
-        # full spmd on loss:
-        mm = [Block(0, span=1, memory=0, btype=FW)]
+        mm = [Block(0, span=1, memory=1, btype=FW)]
         mm_devs = [list(range(ndevs))]
-        # vshape
+        if train:
+            mm += [Block(0, span=1, memory=-1, btype=BW)]
+            mm_devs += [list(range(ndevs))]
+
+        # vshape on branch 1
         ndevs1 = ndevs // 2
-        fbranch1 = [Block(0, span=1, memory=0, btype=FW) for _ in range(ndevs1)]
+        fbranch1 = [Block(0, span=1, memory=1, btype=FW) for _ in range(ndevs1)]
         fdevs = [[devid] for devid in range(ndevs1)]
-        branch1 = fbranch1 + mm
-        branch1_devs = fdevs + mm_devs
-        # vshape
+        bbranch1, bdevs1 = [], []
+        if train:
+            bbranch1 = [Block(0, span=3, memory=-1, btype=BW) for _ in range(ndevs1)]
+            bdevs1 = [[devid] for devid in range(ndevs1)][::-1]
+        branch1 = fbranch1 + mm + bbranch1
+        branch1_devs = fdevs + mm_devs + bdevs1
+
+        # vshape on branch 2
         ndevs2 = ndevs - ndevs1
-        fbranch2 = [Block(0, span=1, memory=0, btype=FW) for _ in range(ndevs1)]
+        fbranch2 = [Block(0, span=1, memory=1, btype=FW) for _ in range(ndevs1)]
         fdevs = [[devid] for devid in range(ndevs1, ndevs1+ndevs2)]
-        branch2 = fbranch2 + mm
-        branch2_devs = fdevs + mm_devs
+        bbranch2, bdevs2 = [], []
+        if train:
+            bbranch2 = [Block(0, span=3, memory=-1, btype=BW) for _ in range(ndevs1)]
+            bdevs2 = [[devid] for devid in range(ndevs1, ndevs1+ndevs2)][::-1]
+        branch2 = fbranch2 + mm + bbranch2
+        branch2_devs = fdevs + mm_devs + bdevs2
     
         sched.add_block_seq(branch1, branch1_devs)
         sched.add_block_seq(branch2, branch2_devs)
@@ -272,6 +241,8 @@ if __name__ == '__main__':
                         help='memory limits')
     parser.add_argument('--save', type=str, default=None,
                         help='save searched schedule under a folder')
+    parser.add_argument('--infer', action='store_true', default=False,
+                        help='search for inference schedule')
     args = parser.parse_args()
 
     print('============== Scheduling Solver ================')
@@ -279,7 +250,7 @@ if __name__ == '__main__':
     sys.stdout.flush()
 
     premise = getattr(Premise, args.premise)
-    micro: SchedPlan = premise(args.ndevs)
+    micro: SchedPlan = premise(args.ndevs, train=not args.infer)
     for gid, blk in enumerate(micro.chain_blocks()):
         blk.gid = gid
 
@@ -288,6 +259,12 @@ if __name__ == '__main__':
     print(micro)
 
     micros = [micro.copy(mid) for mid in range(nmicros)]
+    # for inference, we don't consider memory
+    if args.infer:
+        for micro in micros:
+            for block in micro.all_blocks():
+                block.memory = 0
+
     memory = [args.inflight] * args.ndevs
 
     tic = time.time()
