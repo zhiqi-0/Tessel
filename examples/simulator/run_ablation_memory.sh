b3702c2bb7efc043b38c0aadfc953f540f2c2d47
Zhiqi Lin <zhiqi.0@outlook.com>
2023-07-02 18:27:40 -0700

    update ablation study

--- examples/simulator/run_ablation_memory.sh
+++ examples/simulator/run_ablation_memory.sh
@@ -8,14 +8,24 @@ mkdir -p $LOGS
 set -ex
 
 memory=(1 2 3 4 5 6)
-premise=(vshape xshape mshape yshape)
+
+premise=(vshape)
+nmicros=4
+
+premise=(xshape)
+nmicros=3
+
+premise=(nnshape mshape)
+nmicros=6
+
+premise=(kshape)
+nmicros=3
+
 
 for p in ${premise[@]}; do
     for m in ${memory[@]}; do
-        echo  > ${LOGS}/abalation.tetris.$p.mem$m.log
-
         python examples/simulator/cases_tetris.py \
-            --premise $p --ndevs 4 --nmicros 6 --inflight $m \
-        2>&1 | tee -a ${LOGS}/abalation.tetris.$p.mem$m.log
+            --premise $p --ndevs 4 --nmicros $nmicros --inflight $m \
+        2>&1 | tee ${LOGS}/abalation.tetris.$p.mem$m.log
     done
 done
--- examples/simulator/run_ablation_nmicros.sh
+++ examples/simulator/run_ablation_nmicros.sh
@@ -8,14 +8,13 @@ mkdir -p $LOGS
 set -ex
 
 nmicros=(1 2 3 4 5 6)
-premise=(vshape xshape mshape yshape)
+premise=(vshape xshape kshape mshape nnshape)
+# premise=(nnshape)
 
 for m in ${nmicros[@]}; do
     for p in ${premise[@]}; do
-        echo > ${LOGS}/abalation.tetris.$p.nmicros$m.log
-
         python examples/simulator/cases_tetris.py \
             --premise $p --ndevs 4 --nmicros $m --inflight 10 \
-        2>&1 | tee -a ${LOGS}/abalation.tetris.$p.nmicros$m.log
+        2>&1 | tee ${LOGS}/abalation.tetris.$p.nmicros$m.log
     done
 done
